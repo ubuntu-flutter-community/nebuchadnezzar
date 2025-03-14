d3573870cb49f203795099556feaa110d051cd7a
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2025-03-14 21:48:14 +0100

    fix: usa (#22)

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 export 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 final List<Locale> supportedLocales = {
+  const Locale('US'),
   const Locale('en'),
   const Locale('en_US'),
   ...List.of(AppLocalizations.supportedLocales)..remove(const Locale('en')),
