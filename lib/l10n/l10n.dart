f2b78018d91c706823e73fb1fa2f510c97207184
Feichtmeier <frederik.feichtmeier@gmail.com>
2025-03-15 17:49:34 +0100

    fix: locales

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -3,13 +3,6 @@ import 'package:flutter/widgets.dart';
 
 export 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-final List<Locale> supportedLocales = {
-  const Locale('US'),
-  const Locale('en'),
-  const Locale('en_US'),
-  ...List.of(AppLocalizations.supportedLocales)..remove(const Locale('en')),
-}.toList();
-
 extension LocalizationsContext on BuildContext {
   AppLocalizations get l10n => AppLocalizations.of(this);
 }
