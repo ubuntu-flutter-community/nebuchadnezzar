12ad63c0ca61a03e95b5e15cfe1508fefab891be
Feichtmeier <frederik.feichtmeier@gmail.com>
2025-03-24 18:30:09 +0100

    fix: web links should be clickable
    Fixes #56

---
+++ lib/events/view/chat_event_inspect_dialog.dart
@@ -0,0 +1,105 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:matrix/matrix.dart';
+import 'package:watch_it/watch_it.dart';
+
+import '../../common/chat_model.dart';
+import '../../common/date_time_x.dart';
+import '../../common/view/build_context_x.dart';
+import '../../common/view/chat_avatar.dart';
+import '../../common/view/confirm.dart';
+import '../../common/view/theme.dart';
+import '../../common/view/ui_constants.dart';
+import '../../l10n/l10n.dart';
+
+class ChatEventInspectDialog extends StatelessWidget {
+  const ChatEventInspectDialog({
+    super.key,
+    required this.event,
+    required this.child,
+  });
+
+  final Event event;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    final theme = context.theme;
+    final originalSource = event.originalSource;
+    return ConfirmationDialog(
+      contentPadding: EdgeInsets.zero,
+      title:
+          Text(event.senderFromMemoryOrFallback.displayName ?? event.eventId),
+      content: SizedBox(
+        height: 500,
+        width: 400,
+        child: ListView(
+          children: [
+            ListTile(
+              leading: ChatAvatar(
+                avatarUri: event.senderFromMemoryOrFallback.avatarUrl,
+              ),
+              title: Text(l10n.sender),
+              subtitle: Text(
+                '${event.senderFromMemoryOrFallback.calcDisplayname()} [${event.senderId}]',
+              ),
+            ),
+            ListTile(
+              title: Text('${l10n.time}:'),
+              subtitle:
+                  Text(event.originServerTs.formatAndLocalize(context.l10n)),
+            ),
+            ListTile(
+              title: Text('${l10n.status}:'),
+              subtitle: Text(event.status.name),
+            ),
+            ListTile(title: Text('${l10n.sourceCode}:')),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Material(
+                borderRadius: const BorderRadius.all(kBubbleRadius),
+                color: getTileColor(di<ChatModel>().isUserEvent(event), theme),
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(16),
+                  scrollDirection: Axis.horizontal,
+                  child: SelectableText(
+                    improveJson(MatrixEvent.fromJson(event.toJson())),
+                    style: TextStyle(
+                      color: theme.colorScheme.onSurface,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            if (originalSource != null) ...[
+              ListTile(title: Text('${l10n.encrypted}:')),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Material(
+                  borderRadius: const BorderRadius.all(kBubbleRadius),
+                  color:
+                      getTileColor(di<ChatModel>().isUserEvent(event), theme),
+                  child: SingleChildScrollView(
+                    padding: const EdgeInsets.all(16),
+                    scrollDirection: Axis.horizontal,
+                    child: SelectableText(
+                      improveJson(originalSource),
+                      style: TextStyle(
+                        color: theme.colorScheme.onSurface,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+
+  String improveJson(MatrixEvent event) => const JsonEncoder.withIndent('    ')
+      .convert(const JsonDecoder().convert(jsonEncode(event.toJson())));
+}
--- lib/events/view/chat_text_message.dart
+++ lib/events/view/chat_text_message.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
 import 'package:matrix/matrix.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+import '../../common/view/confirm.dart';
+import '../../l10n/l10n.dart';
 import 'chat_html_message.dart';
 
 class ChatTextMessage extends StatelessWidget {
@@ -22,11 +26,26 @@ class ChatTextMessage extends StatelessWidget {
           room: event.room,
           style: messageStyle,
         )
-      : Text.rich(
-          TextSpan(
-            style: messageStyle,
-            text: displayEvent.body,
-          ),
+      : Linkify(
+          text: displayEvent.body,
           style: messageStyle,
+          onOpen: (link) {
+            final maybe = Uri.tryParse(link.url);
+            if (maybe != null) {
+              showDialog(
+                context: context,
+                builder: (context) => ConfirmationDialog(
+                  title: Text(
+                    '${context.l10n.openLinkInBrowser}?',
+                  ),
+                  content: SizedBox(
+                    width: 400,
+                    child: Text(link.toString()),
+                  ),
+                  onConfirm: () => launchUrl(maybe),
+                ),
+              );
+            }
+          },
         );
 }
