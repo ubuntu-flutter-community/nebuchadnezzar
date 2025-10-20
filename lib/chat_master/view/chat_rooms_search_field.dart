2a1539cf3ef60bd433c07633703eaf19a1c8aece
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2025-10-21 01:23:47 +0200

    fix: reload room filter query (#114)

--- lib/chat_master/view/chat_rooms_search_field.dart
+++ lib/chat_master/view/chat_rooms_search_field.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:watch_it/watch_it.dart';
+import 'package:yaru/icons.dart';
 
 import '../../common/chat_manager.dart';
+import '../../common/view/theme.dart';
 import '../../common/view/ui_constants.dart';
 import '../../l10n/l10n.dart';
 
-class ChatRoomsSearchField extends StatelessWidget {
+class ChatRoomsSearchField extends StatefulWidget {
   const ChatRoomsSearchField({super.key});
+
+  @override
+  State<ChatRoomsSearchField> createState() => _ChatRoomsSearchFieldState();
+}
+
+class _ChatRoomsSearchFieldState extends State<ChatRoomsSearchField> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    if (di<ChatManager>().filteredRoomsQuery != null) {
+      _controller.text = di<ChatManager>().filteredRoomsQuery!;
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) => Padding(
@@ -16,10 +39,19 @@ class ChatRoomsSearchField extends StatelessWidget {
       bottom: kMediumPadding,
     ),
     child: TextField(
+      controller: _controller,
       onChanged: di<ChatManager>().setFilteredRoomsQuery,
       decoration: InputDecoration(
         hintText: context.l10n.search,
         label: Text(context.l10n.search),
+        suffixIcon: IconButton(
+          style: textFieldSuffixStyle,
+          onPressed: () {
+            _controller.clear();
+            di<ChatManager>().setFilteredRoomsQuery(null);
+          },
+          icon: const Icon(YaruIcons.edit_clear),
+        ),
       ),
       autofocus: true,
     ),
